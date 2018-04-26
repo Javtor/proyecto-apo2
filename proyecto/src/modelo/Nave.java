9bd3ef7fa717e589b35cb09b64a658e909cf9e43
camilaleniss <mlmariacami2@gmail.com>
2018-04-26 07:59:20 -0500

    R6 hecho (metodo darlistajugadores de la clase Juego)

--- proyecto/src/modelo/Nave.java
+++ proyecto/src/modelo/Nave.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyEvent;
 public class Nave extends SpriteMovimiento implements Colisionable {
 	
 	public static final int VELOCIDAD_BASE = 5;
-	public static final int VIDAS = 3;
+	public static final int VIDAS_INICIAL = 3;
 	
 	private int vidas;
 	
@@ -13,7 +13,7 @@ public class Nave extends SpriteMovimiento implements Colisionable {
 		super(Juego.ANCHO/2, Juego.ALTO/2, "img/lennon.png");
 		setX(getX()-getAncho()/2);
 		setY(getY()-getAlto()/2);
-		vidas = VIDAS;
+		vidas = VIDAS_INICIAL;
 	}
 
 	public void disminuirvida() {
