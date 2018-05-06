48a531411ee8114e6cbaa43b00d9fbf3e8015185
Javier Torres <javierandrestorresreyes@gmail.com>
2018-05-06 12:45:08 -0500

    proyectil

--- proyecto/src/modelo/Proyectil.java
+++ proyecto/src/modelo/Proyectil.java
@@ -1,32 +1,41 @@
 package modelo;
 
 import java.awt.geom.Rectangle2D;
+import java.io.File;
 
 public class Proyectil extends SpriteMovimiento implements Colisionable {
-	
-	public static final int VELOCIDAD = 10;
-	private Proyectil siguiente;
 
-	public Proyectil(int x, int y, String imagen, int x2, int y2) {
-		super(x, y, imagen);
-		calcularVelocidades(x2,y2);
-	}
+	public static final int VELOCIDAD = 15;
+	public static final String UBICACION = "img" + File.separator + "proyectil.jpg";
+	public static final int DANIO = 1;
 
-	private int daño;
+	private int danio;
 	private char tipo;
-	
-	public void calcularVelocidades(int x2, int y2) {
-		int difX = x2-getX();
-		int difY = y2-getY();
-		double hip = Math.sqrt(difX*difX+difY*difY);
-		double prop = VELOCIDAD/hip;
-		setDX((int)(prop*(x2-getX())));
-		setDY((int)(prop*(y2-getY())));
+
+	public Proyectil(int x, int y) {
+		super(x, y, UBICACION);
+		danio = DANIO;
+		setVisible(false);
 	}
-	
+
+	public void disparar(int x, int y, int x2, int y2) {
+		if (!esVisible()) {
+			setX(x);
+			setY(y);
+			int difX = x2 - getX();
+			int difY = y2 - getY();
+			double hip = Math.sqrt(difX * difX + difY * difY);
+			double prop = VELOCIDAD / hip;
+			setDX((int) (prop * (x2 - getX())));
+			setDY((int) (prop * (y2 - getY())));
+		}
+	}
+
 	@Override
 	public void colisionaCon(Colisionable c) {
-
+		if(c instanceof Pelota) {
+			setVisible(false);
+		}
 	}
 
 	@Override
@@ -38,4 +47,14 @@ public class Proyectil extends SpriteMovimiento implements Colisionable {
 	public Rectangle2D getHitbox() {
 		return new Rectangle2D.Double(getX(), getY(), getAncho(), getAlto());
 	}
+	
+
+	public int getDanio() {
+		return danio;
+	}
+
+	public void setDanio(int danio) {
+		this.danio = danio;
+	}
+	
 }
