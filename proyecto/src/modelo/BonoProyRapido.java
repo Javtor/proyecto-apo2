91331f926908a1df1647b7302ee2230574ca5dd8
Javier Torres <javierandrestorresreyes@gmail.com>
2018-05-15 09:40:55 -0500

    Añadida herencia en bonificaciones

---
+++ proyecto/src/modelo/BonoProyRapido.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class BonoProyRapido extends Bonificacion {
+	public static String DIREC = "img/bonus/proyectil_r.png";
+	
+	public BonoProyRapido() {
+		super();
+		setImagen(DIREC);
+	}
+}
---
+++ proyecto/src/modelo/BonoVida.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class BonoVida extends Bonificacion{
+	
+	public static String DIREC = "img/bonus/bonusvida.png";
+	
+	public BonoVida() {
+		super();
+		setImagen(DIREC);
+	}
+
+}
