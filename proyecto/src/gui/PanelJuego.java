7e57274419054da427a73d077b470de086dbc7e7
Javier Torres <javierandrestorresreyes@gmail.com>
2018-05-18 19:45:26 -0500

    Añadido Antialiasing al dibujar la luna

--- proyecto/src/gui/PanelJuego.java
+++ proyecto/src/gui/PanelJuego.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -46,6 +47,8 @@ public class PanelJuego extends JPanel implements KeyListener, MouseListener {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
 		if ((principal.isJugando())) {
 			dibujarJuego(g2);
 		} else {
